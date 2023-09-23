1a1aecbf6116df6c07a494667616ba07fca97915
도경 <dokyungkim2926@gmail.com>
2023-09-23 23:18:11 +0900

    .

---
+++ demo/src/main/java/com/example/demo/global/oauth/info/OAuthUserInfoFactory.java
@@ -0,0 +1,25 @@
+package com.example.demo.global.oauth.info;
+
+import com.example.demo.domain.user.domain.ProviderType;
+import com.example.demo.global.oauth.info.impl.GoogleOAuthUserInfo;
+import com.example.demo.global.oauth.info.impl.KakaoOAuthUserInfo;
+import lombok.AllArgsConstructor;
+
+import javax.validation.constraints.Null;
+import java.util.Map;
+
+@AllArgsConstructor
+public class OAuthUserInfoFactory {
+    public static OAuthUserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
+
+        switch (providerType) {
+            case GOOGLE:
+                return GoogleOAuthUserInfo.ofGoogle(attributes);
+            case KAKAO:
+                return KakaoOAuthUserInfo.ofKakao(attributes);
+            default:
+                throw new IllegalArgumentException("Not");
+        }
+
+    }
+}
