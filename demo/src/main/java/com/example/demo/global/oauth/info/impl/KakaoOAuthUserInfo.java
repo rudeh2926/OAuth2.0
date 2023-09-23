6617753506c7ec2e719b79ad8800dfd05d1bf558
도경 <dokyungkim2926@gmail.com>
2023-09-23 23:08:10 +0900

    Kakao Impl

---
+++ demo/src/main/java/com/example/demo/global/oauth/info/impl/KakaoOAuthUserInfo.java
@@ -0,0 +1,21 @@
+package com.example.demo.global.oauth.info.impl;
+
+import com.example.demo.global.oauth.info.OAuthUserInfo;
+
+import java.util.Map;
+
+public class KakaoOAuthUserInfo extends OAuthUserInfo {
+    public KakaoOAuthUserInfo (Map<String, Object> attributes, String nameAttributeKey, String name, String password, String email) {
+        super(attributes, nameAttributeKey, name, password, email);
+    }
+
+    public static OAuthUserInfo ofKakao(Map<String, Object> attributes) {
+        return KakaoOAuthUserInfo.builder()
+                .name((String) attributes.get("name"))
+                .password((String) attributes.get("password"))
+                .email((String) attributes.get("email"))
+                .attributes(attributes)
+                .nameAttributeKey("sub")
+                .build();
+    }
+}
